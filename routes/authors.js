846612ca1dea9e24f2388bb367b6d52256e72f4c
John Carney <jcarney000@gmail.com>
2016-06-28 21:52:44 -0700

    add authors and books routes; prepare for heroku deploy

--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,94 @@
 'use strict';
 
+const knex = require('../knex');
 const express = require('express');
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/authors', (_req, res, next) => {
+  knex('authors')
+    .orderBy('id')
+    .then((authors) => {
+      res.send(authors);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/authors/:id/books', (req, res, next) => {
+  knex('books')
+    .where('author_id', req.params.id)
+    .orderBy('id')
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/authors/:id', (req, res, next) => {
+  knex('authors')
+    .where('id', req.params.id)
+    .first()
+    .then((artist) => {
+      if (!artist) {
+        return next();
+      }
+      res.send(artist);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/authors', (req, res, next) => {
+  knex('authors')
+    .insert(req.body, '*')
+    .then((results) => {
+      res.send(results[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/authors/:id', (req, res, next) => {
+  knex('authors')
+    .update(req.body, '*')
+    .where('id', req.params.id)
+    .then((results) => {
+      if (results.length === 0) {
+        return next();
+      }
+
+      res.send(results[0]);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/authors/:id', (req, res, next) => {
+  knex('authors')
+    .where('id', req.params.id)
+    .first()
+    .then((author) => {
+      if (!author) {
+        return next();
+      }
+
+      return knex('authors')
+      .del()
+      .where('id', req.params.id)
+      .then((authors) => {
+        delete author.id;
+        res.send(author);
+      })
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
 
 module.exports = router;
